66ae09d78e1e16ff08dd669992094d194e8615c9
sosag <sosag@GAEL>
2025-02-19 18:23:37 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interfaz_para_distancia_de_2_puntos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x1=double.Parse(textBox1.Text);
+            double x2 = double.Parse(textBox2.Text);
+            double y1 = double.Parse(textBox3.Text);
+            double y2 = double.Parse(textBox4.Text);
+            double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            textBox5.Text = d.ToString();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
